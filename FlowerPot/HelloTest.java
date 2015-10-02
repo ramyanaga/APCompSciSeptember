17c694d80bffb437c1dea158dd6608cb74482700
ramyanaga <rrramya@Ramyas-MBP.att.net>
2015-10-02 05:39:10 -0700

    Flower Pot Code
    This code prints flower pots

---
+++ FlowerPot/HelloTest.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class HelloTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloTest
+{
+    public static void main(String[] args) {
+        for (int i = 0; i <3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("hello"); 
+            }
+        System.out.println(""); 
+    }
+    
+}
+}
+    
